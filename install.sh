7dfea63fe00aaaab58146a9cbc3c93a63dc65dbc
Bodo Bauer <bb@bb-zone.com>
2026-09-20 17:36:18 +0200

    Fix

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir -p ${ubin}
 install -o root -g root -m 0750 -t ${ubin} HID-Setup/hid-gadget-stop.sh HID-Setup/hid-gadget-start.sh
 
 # Make systemd aware of new service
-install -o root -g root -m 0640 -t /etc/systemd/system install.sh
+install -o root -g root -m 0640 -t /etc/systemd/system HID-Setup/hid-gadget.service
 
 # reload systemd to pick up new sevice
 systemctl daemon-reload
